c3f6a1ec07d8667bcc3e3a44ac80054462e2859a
terrenal07 <ikerberme@gmail.com>
2023-07-05 13:03:47 +0200

    foto 1

---
+++ js/codigo.js
@@ -0,0 +1,12 @@
+
+
+function ampliar() {
+    var foto = document.getElementById("foto");
+    foto.style.width = "300px";
+}
+
+function reducir() {
+    var foto = document.getElementById("foto");
+    foto.style.width = "200px";
+}
+
